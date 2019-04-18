46fd4206745c22aacd5ae12ac123815b717e9102
AliAbdulle <alidhobey@gmail.com>
2019-04-18 10:14:28 -0500

    saved work type

--- src/components/owners/OwnerList.js
+++ src/components/owners/OwnerList.js
@@ -14,7 +14,7 @@ export default class OwnerList  extends Component {
                          </p>
                          <button
                                 onClick={() => this.props.deleteOwner(owner.id)}
-                                className="card-link">Delete</button>
+                                className="card-link">Delete Owners</button>
 
                     </div>
                     )
